764e24694ac84149ab88a14138fafc6bf7bb615d
Maty910 <matychacong@gmail.com>
2024-08-10 14:25:15 -0300

    feat: fav icon

---
+++ src/components/Aside.jsx
@@ -0,0 +1,7 @@
+export const () => {
+    return (
+        <aside>
+        
+        </aside>
+    )
+}
--- src/const.js
+++ src/const.js
@@ -1,6 +1,6 @@
 export const TURNS = {
     X: '✕',
-    O: '◯'
+    O: '〇'
 }
   
 export const WINNER_COMBOS = [
